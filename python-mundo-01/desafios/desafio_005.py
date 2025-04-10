7a35176c3301c809148eb83d5fc9fc1fbbe66d62
lia-oliveira <dev.gillian@gmail.com>
2025-04-10 18:15:53 -0300

    Solucao do desafio 005

---
+++ python-mundo-01/desafios/desafio_005.py
@@ -0,0 +1,14 @@
+# ================================
+#        Desafio 005
+# ================================
+
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n_int = int(input('Digite um número inteiro: '))
+
+antecessor = n_int - 1
+sucessor = n_int + 1
+
+print('Número:', n_int)
+print('Antecessor:', antecessor)
+print('Sucessor:', sucessor)
