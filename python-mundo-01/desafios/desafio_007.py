9a1e6814aff1b7bf635baf13a2b0f730cc32e469
lia-oliveira <dev.gillian@gmail.com>
2025-04-10 18:57:28 -0300

    Resolucao do desafio 7

---
+++ python-mundo-01/desafios/desafio_007.py
@@ -0,0 +1,13 @@
+# ================================
+#        Desafio 007
+# ================================
+
+
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'Média: {media:.2f}')
