f6d1fadf78ae550d80791ff3050ac527b080aae1
lia-oliveira <dev.gillian@gmail.com>
2025-04-11 21:59:40 -0300

    Solucao do desafio 13

---
+++ python-mundo-01/desafios/desafio_013.py
@@ -0,0 +1,11 @@
+# ================================
+#        Desafio 013
+# ================================
+
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.
+
+salario = float(input('Digite o salário: R$ '))
+
+aumento = salario * 1.15;
+
+print(f'Salário com 15% de aumento: R$ {aumento:.2f}')
