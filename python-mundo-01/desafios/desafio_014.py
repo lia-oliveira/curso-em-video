f222034e8873219592e3e9f2735c7b5c9cd16b1c
lia-oliveira <dev.gillian@gmail.com>
2025-04-13 18:06:53 -0300

    Temperatura em Celsius para Fahrenheit

---
+++ python-mundo-01/desafios/desafio_014.py
@@ -0,0 +1,11 @@
+# ================================
+#        Desafio 014
+# ================================
+
+# Escreva um programa que converta uma temperatura digitada em "°C" para "°F".
+
+temperatura = float(input('Digite a temperatura: '))
+
+temperaturaF = temperatura * 1.8 + 32
+
+print(f"Temeperatura e \u00B0F: {temperaturaF}\u00B0F")
